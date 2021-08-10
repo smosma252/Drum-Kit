7db461bb41fdc324faf28fa19e37da47af735569
smosma252 <osama_o.s@hotmail.com>
2021-08-10 16:59:12 -0400

    main file

---
+++ index.js
@@ -0,0 +1,73 @@
+function instrument(character) {
+
+    switch (character) {
+        case "w":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        case "a":
+            var kick1 = new Audio('sounds/kick-bass.mp3');
+            kick1.play();
+            break;
+
+        case "s":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+
+        case "d":
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+
+
+        case "j":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+
+        case "k":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+
+        case "l":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+
+        default:
+    }
+
+
+}
+
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
+
+
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var buttonInnerHtml = this.innerHTML;
+        instrument(buttonInnerHtml);
+        buttonAnimation(buttonInnerHtml);
+    });
+}
+
+document.addEventListener("keydown", function (event) {
+    instrument(event.key);
+    buttonAnimation(event.key);
+})
+
+
+
